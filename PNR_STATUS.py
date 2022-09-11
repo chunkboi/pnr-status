cae2b79b2847b1b7fd778b5ed0c8e12d0f634f06
chunk <comicsgr@gmail.com>
2022-09-11 08:42:07 +0000

    Update PNR_STATUS.py
    Add MakeMyTrip code

--- PNR_STATUS.py
+++ PNR_STATUS.py
@@ -1,77 +1,64 @@
-from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
-from cryptography.hazmat.primitives import padding
-from cryptography.hazmat.backends import default_backend
-from base64 import b64decode, b64encode
 from requests import post
+from subprocess import call
 from json import loads
-import time
-import sys
+from os import name
+from time import time
 
 def clear():
-    #clear screen by printing tons of newlines
-    print('\n'* 1000)
-clear()
+    #clear screen
+    call(["clear"] if name == 'posix' else ["cls"], shell=True)
+  
 
-# encrypt pnr with key and iv stole it from https://stackoverflow.com/questions/50062663/encryption-decryption-using-aes-cbc-pkcs7padding
-def getEncryptedPNR(pnr):
-    data = bytes(pnr, 'utf-8')
-    backend = default_backend()
-    padder = padding.PKCS7(128).padder()
-    
-    data = padder.update(data) + padder.finalize()
-    key = b'8080808080808080'
-    iv = b'8080808080808080'
-    cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=backend)
-    encryptor = cipher.encryptor()
-    ct = encryptor.update(data) + encryptor.finalize()
-    enc_pnr = b64encode(ct)
-    return enc_pnr.decode('utf-8')
-
- # format response json and print it
 def printData(json_data):
-    boardingStation = json_data["BrdPointName"]
-    destinationStation = json_data["DestStnName"]
-    quota = json_data["quota"]
-    className = json_data["className"]
-    trainNumber = json_data["trainNumber"]
-    trainName = json_data["trainName"]
-    dateOfJourney = json_data["dateOfJourney"]    
+    train_number = json_data["TrainDetails"]["Train"]["Number"]
+    train_name = json_data["TrainDetails"]["Train"]["Name"]
+    chart_status = json_data["TrainDetails"]["ChartPrepared"]
+    start_station = json_data["StationDetails"]["BoardingPoint"]["name"]
+    destination_station = json_data["StationDetails"]["ReservationUpto"]["name"]
+    date_of_journey = json_data["PnrDetails"]["SourceDoj"]["FormattedDate"]
+    class_name = json_data["PnrDetails"]["Class"]
+    quota = json_data["PnrDetails"]["Quota"]
     
     print("PNR STATUS")
     print("------------------------------------------------------------------")
-    print(boardingStation + " -> " + destinationStation) # source and destination station
-    print(trainNumber + ' - ' + trainName) # train number and name
+    print(start_station + " -> " + destination_station)
+    print(train_number + " - " + train_name)
     print()
+    print("Chart Prepared?: " + str(chart_status))
     print("Quota: " + quota)
-    print("Journey Class: " + className)
-    print("Date Of Journey: " + dateOfJourney)
+    print("Class: " + class_name)
+    print("Date Of Journey: " + date_of_journey)
     print()
-    for p in json_data["passengerList"]:
-        print("Passenger " + p["passengerSerialNumber"] + ": " + p["currentStatus"] + '/' + p["currentCoachId"] + '/' + p["currentBerthNo"])
+    for p in json_data["PassengerDetails"]["PassengerStatus"]:
+        print("Passenger " + str(p["Number"]) + ": " + p["CurrentStatus"])
+    
+    
+    
 
+headers = {
+    'accept': 'application/json',
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36',   
+}
 
-
-pnr = input("Enter PNR Number: ") #  recieve PNR number
-start = time.time()
-
-#input validation pnr should be 10 digits
-if len(pnr) != 10:
+pnr = input("Enter PNR number: ")
+start = time()
+if len(pnr) != 10: # pnr validation
     print("PNR LENGTH should be 10 DIGITS")
     sys.exit(0)
    
     
-json_data = {
-    'pnrNumber': getEncryptedPNR(pnr),
+json = {
+    'pnrID': pnr,
+    'trackingParams': {
+        'affiliateCode': 'MMT001',
+        'channelCode': 'WEB',
+    },
 }
 
+response = post('https://mapi.makemytrip.com/api/rails/pnr/currentstatus/v1',headers=headers, json=json)
 
-# do a post request to the url with encrypted pnr
-response = post('https://railways.easemytrip.com/Train/PnrchkStatus', json=json_data)
-json_data = loads(response.content) # parse response content into json
-
-
+json_data = loads(response.content)
+end = time()
 clear()
-end = time.time()
 printData(json_data)
 print(f"Total time taken: {round(end - start, 3)} seconds") # print total time taken to complete the program
-
